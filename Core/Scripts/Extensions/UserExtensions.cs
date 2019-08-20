cf445029443af4e7dc985a5e74bb1bff69aea756
Yaroslav Lahin <tiarwe@gmail.com>
2019-08-20 12:24:53 +0300

    Added the RefreshAsync extension for the IUser interface

--- Core/Scripts/Extensions/UserExtensions.cs
+++ Core/Scripts/Extensions/UserExtensions.cs
@@ -9,6 +9,25 @@ namespace Creobit.Backend
 
         private const int MillisecondsDelay = 10;
 
+        public static async Task RefreshAsync(this IUser self)
+        {
+            var invokeResult = default(bool?);
+
+            self.Refresh(
+                () => invokeResult = true,
+                () => invokeResult = false);
+
+            while (!invokeResult.HasValue)
+            {
+                await Task.Delay(MillisecondsDelay);
+            }
+
+            if (!invokeResult.Value)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
         public static async Task SetNameAsync(this IUser self, string name)
         {
             var invokeResult = default(bool?);
